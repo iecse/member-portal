7cb0d8a744709d13aceeb87fd54fcf574758b152
pranavtharoor <pranav.tharoor@gmail.com>
2018-09-07 13:28:27 +0530

    Modified config

--- config/endpoints.js
+++ config/endpoints.js
@@ -1,4 +1,7 @@
-export const api = 'http://localhost:3000';
+export const api =
+  process.env.NODE_ENV === 'production'
+    ? 'https://api.iecsemanipal.com'
+    : 'http://localhost:3000';
 export const charts = 'http://charts.iecsemanipal.com';
 export default {
   api,
