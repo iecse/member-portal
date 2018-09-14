f21206a57d046ba185698e0a121a8b5806129798
pranavtharoor <pranav.tharoor@gmail.com>
2018-09-14 19:46:17 +0530

    Modified test

--- src/core/App/app.spec.js
+++ src/core/App/app.spec.js
@@ -4,7 +4,7 @@ import { shallow } from 'enzyme';
 
 describe('<App />', () => {
   it('renders 1 <App /> component', () => {
-    const component = shallow(<App loggedIn={false} />);
+    const component = shallow(<App loggedIn={false} loading={false} />);
     expect(component).toHaveLength(1);
   });
 });
