3284c5208dd91a8e4ca6a30f86005d7036015f18
pranavtharoor <pranav.tharoor@gmail.com>
2018-09-14 17:42:26 +0530

    Added triangles to navbar

--- src/modules/Navbar/Navbar.js
+++ src/modules/Navbar/Navbar.js
@@ -29,83 +29,87 @@ class Navbar extends Component {
     return (
       <div className={`navbar-container ${this.state.expanded && 'expanded'}`}>
         <div className="card">
-          <img
-            className="logo"
-            src="https://iecsemanipal.com/images/logos/iecse-logo-white.png"
-          />
-          <div className={`nav ${this.state.open && 'open'}`}>
-            <div className={`navbar`}>
-              <button
-                className={this.state.active === '/' ? 'active' : ''}
-                onClick={() => {
-                  this.props.history.push('/');
-                  this.setState({ active: '/', expanded: true, open: false });
-                }}
-              >
-                Home
-              </button>
-              <button
-                className={this.state.active === '/events' ? 'active' : ''}
-                onClick={() => {
-                  this.props.history.push('/events');
-                  this.setState({
-                    active: '/events',
-                    expanded: false,
-                    open: false
-                  });
-                }}
-              >
-                Events
-              </button>
-              <button
-                className={this.state.active === '/account' ? 'active' : ''}
-                onClick={() => {
-                  this.props.history.push('/account');
-                  this.setState({
-                    active: '/account',
-                    expanded: false,
-                    open: false
-                  });
-                }}
-              >
-                Account
-              </button>
-              {this.props.type >= 20 && (
+          <div>
+            <img
+              className="logo"
+              src="https://iecsemanipal.com/images/logos/iecse-logo-white.png"
+            />
+            <div className={`nav ${this.state.open && 'open'}`}>
+              <div className={`navbar`}>
                 <button
-                  className={this.state.active === '/register' ? 'active' : ''}
+                  className={this.state.active === '/' ? 'active' : ''}
                   onClick={() => {
-                    this.props.history.push('/register');
+                    this.props.history.push('/');
+                    this.setState({ active: '/', expanded: true, open: false });
+                  }}
+                >
+                  Home
+                </button>
+                <button
+                  className={this.state.active === '/events' ? 'active' : ''}
+                  onClick={() => {
+                    this.props.history.push('/events');
                     this.setState({
-                      active: '/register',
+                      active: '/events',
                       expanded: false,
                       open: false
                     });
                   }}
                 >
-                  Register
+                  Events
                 </button>
-              )}
-              <button onClick={this.props.logout}>Log Out</button>
-            </div>
-          </div>
-          <div
-            onClick={() => this.setState({ open: !this.state.open })}
-            className={`hamburger-icon ${this.state.open && 'open'}`}
-          >
-            <div />
-            <div />
-            <div />
-          </div>
-          {this.state.expanded && (
-            <div>
-              <div className="greeting">
-                Hello {this.props.userDetails.name.split(' ')[0]}.
+                <button
+                  className={this.state.active === '/account' ? 'active' : ''}
+                  onClick={() => {
+                    this.props.history.push('/account');
+                    this.setState({
+                      active: '/account',
+                      expanded: false,
+                      open: false
+                    });
+                  }}
+                >
+                  Account
+                </button>
+                {this.props.type >= 20 && (
+                  <button
+                    className={
+                      this.state.active === '/register' ? 'active' : ''
+                    }
+                    onClick={() => {
+                      this.props.history.push('/register');
+                      this.setState({
+                        active: '/register',
+                        expanded: false,
+                        open: false
+                      });
+                    }}
+                  >
+                    Register
+                  </button>
+                )}
+                <button onClick={this.props.logout}>Log Out</button>
               </div>
-              <div className="greeting-sub">
-                Welcome to IECSE member portal.
-              </div>
             </div>
-          )}
+            <div
+              onClick={() => this.setState({ open: !this.state.open })}
+              className={`hamburger-icon ${this.state.open && 'open'}`}
+            >
+              <div />
+              <div />
+              <div />
+            </div>
+            {this.state.expanded && (
+              <div>
+                <div className="greeting">
+                  Hello {this.props.userDetails.name.split(' ')[0]}.
+                </div>
+                <div className="greeting-sub">
+                  Welcome to IECSE member portal.
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     );
